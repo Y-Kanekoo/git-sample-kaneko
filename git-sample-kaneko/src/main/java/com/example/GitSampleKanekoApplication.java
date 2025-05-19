fb961130cc338309f379ad3046a226912b93a7bc
Y-Kanekoo <kaneko.yuki2001@gmail.con>
2025-05-19 11:52:24 +0900

    first commit

---
+++ git-sample-kaneko/src/main/java/com/example/GitSampleKanekoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleKanekoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleKanekoApplication.class, args);
+	}
+
+}
